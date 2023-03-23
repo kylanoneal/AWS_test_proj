9c735d5efc07cd0999e9fa4ea947e499c273f41b
John Caraway <jcarawa2@uncc.edu>
2023-03-23 15:31:30 -0400

    adding second algorithm to ML_Models.py

--- project_app/ML_models.py
+++ project_app/ML_models.py
@@ -47,13 +47,48 @@ def algo_2(input_text):
     '''determine which sentences are most relevant by using bags of words.
     This will create a dictionary of every meaningful word in the text and rate each sentence by how similar it is to the overall text.
     For example, a sentence containing "pancakes" is likely a better summary to a text about making pancakes than a sentence without the word "pancakes".'''
-    sentences, words = read_data(input_text)
+    sentences, words = read_data(input_text) #break text into sentences and total words
 
-#used for testing
-'''def main():
-    test_text = """
- Muad'Dib learned rapidly because his first training was in how to learn. And the first lesson of all was the basic trust that he could learn. It's shocking to find how many people do not believe they can learn, and how many more believe learning to be difficult."""
-    algo_1(test_text)
+    stemmer = nltk.stem.SnowballStemmer('english', ignore_stopwords=True) #stemming attempts to remove tense and differentiate between words that may have the same root, but different meanings
+
+    sentences_stemmed = [[stemmer.stem(word) for word in word_tokenize(sentence)] for sentence in sentences] #stem each sentence in text
+    words_stemmed = [stemmer.stem(word) for word in words] #stem each word in text
+    #print('words', words_stemmed) #want to remove periods and commas
+    #print('sentences', sentences_stemmed)
+
+    total_bow = gen_bag_of_words(words_stemmed) #create histogram of each word in the entire text
+    sentence_bows = [gen_bag_of_words(sentence) for sentence in sentences_stemmed] #create histogram for each word in each sentence
+
+    bag_distances = [sum([total_bow[key] - sentence[key] for key in sentence.keys()]) for sentence in sentence_bows] #calculate basic distance between histograms; may want to alter this calculation because negative values could be a problem
+    #print(bag_distances)
+
+    best_sentence = bag_distances.index(min(bag_distances)) #calc index of best sentence
+    #print(sentences[best_sentence])
+    title = sentences[0]
+
+    return title, sentences[best_sentence]
+
+#generate bag of words for algo_2
+def gen_bag_of_words(words: list):
+    bow_dict = {} #bag of words for input; bag of words is a histogram that counts each occurance of a word in the text
+
+    for word in words: #loop through each word
+
+        if word in bow_dict.keys(): #if the key exists iterate count by 1
+            bow_dict[word] += 1
+        else: #if key doesn't exist, add key to dict
+            bow_dict[word] = 1
+
+    bow_dict.pop('.') #currently removing some punctuation
+    bow_dict.pop(',')
+    return bow_dict
+
+'''#used for testing
+def main():
+    #test_text = """
+ #Muad'Dib learned rapidly because his first training was in how to learn. And the first lesson of all was the basic trust that he could learn. It's shocking to find how many people do not believe they can learn, and how many more believe learning to be difficult."""
+    test_text = "This This This, This This This. This This This, This, This This."
+    algo_2(test_text)
 
 if __name__ == "__main__":
     main()'''
