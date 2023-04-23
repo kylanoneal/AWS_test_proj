3febd368a53dc54e12a5cd24b701be2bcd8d342e
Kylan O'Neal <kylanoneal@gmail.com>
2023-04-23 15:26:36 -0400

    Using different pdf library

--- project_app/text_from_file.py
+++ project_app/text_from_file.py
@@ -1,6 +1,6 @@
 from PIL import Image
 from pytesseract import pytesseract
-from PyPDF2 import PdfReader
+import fitz
 import docx
 
 #needed for running tesseract on windows machine
@@ -8,24 +8,29 @@ import docx
 #pytesseract.tesseract_cmd = tesseract_path
 
 
+
 def get_text_from_text_file(filename):
-    if filename[:-4] == ".txt":
+
+    if filename[-4:] == ".txt":
         file = open(filename, 'r')
+
         return file.read()
-    elif filename[:-5] == ".docx":
+    elif filename[-5:] == ".docx":
         doc = docx.Document(filename)
         text = ""
+
         for para in doc.paragraphs:
-            text += para
+            text += para.text
+
         return text
-    elif filename[:-4] == ".pdf":
-        reader = PdfReader(filename)
-
+    elif filename[-4:] == ".pdf":
+        doc = fitz.open(filename)
         text = ""
-        for page in reader.pages:
-            text += page.extract_text()
 
-        return page
+        for page in doc:
+            text += page.get_text()
+
+        return text
     else:
         return "Invalid file format."
 
